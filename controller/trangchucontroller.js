14f486378ceb1641133a9638b8c9c73d3c0d252f
Phan Duc <phanduchupdkh@gmail.com>
2021-04-21 13:52:30 +0700

    change trnag chu

--- controller/trangchucontroller.js
+++ controller/trangchucontroller.js
@@ -3,91 +3,136 @@ let Productserver = require('../models/productservermodel.js');
 let Diachi = require('../ntcmodules/tradiachimodule.js');
 let tolink = require('../validate/xoadauchuyenlink.js');
 
-module.exports.index = async function(req, res){
+module.exports.index = async function (req, res) {
+	const ssp = 6;
+	let boganmat = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Bổ Gan Mật" } },
+		{ $limit: ssp }
+	])
+	let bomoctoc = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Bổ Mọc Tóc" } },
+		{ $limit: ssp }
+	])
+	let comchotrebiengan = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Cốm Cho Trẻ Biếng Ăn" } },
+		{ $limit: ssp }
+	])
+	let collagen = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Collagen" } },
+		{ $limit: ssp }
+	])
 
-let boganmat = await Productserver.find({"loaisanpham" : "Bổ Gan Mật"});
-let bomoctoc = await Productserver.find({"loaisanpham" : "Bổ Mọc Tóc"});
-let comchotrebiengan = await Productserver.find({"loaisanpham" : "Cốm Cho Trẻ Biếng Ăn"});
-let collagen = await Productserver.find({"loaisanpham" : "Collagen"});
-let botrinao = await Productserver.find({"loaisanpham" : "Bổ Trí Não"});
-let bomat = await Productserver.find({"loaisanpham" : "Bổ Mắt"});
-let boxuongkhop = await Productserver.find({"loaisanpham" : "Bổ Xương Khớp"});
-let duongtieuhoa = await Productserver.find({"loaisanpham" : "Đường Tiêu Hóa"});
-let giamcanantoan = await Productserver.find({"loaisanpham" : "Giảm Cân An Toàn"});
-let sinhlynam = await Productserver.find({"loaisanpham" : "Sinh Lý Nam"});
-let sinhlynu = await Productserver.find({"loaisanpham" : "Sinh Lý Nữ"});
-let tieuduongmomau = await Productserver.find({"loaisanpham" : "Tiểu Đường Mỡ Máu"});
-let thannieu = await Productserver.find({"loaisanpham" : "Thận Niệu"});
-let timmachhuyetap = await Productserver.find({"loaisanpham" : "Tim Mạch Huyết Áp"});
-let tangcanantoan = await Productserver.find({"loaisanpham" : "Tăng Cân An Toàn"});
-let viemduonghohap = await Productserver.find({"loaisanpham" : "Viêm Đường Hô Hấp"});
-let nhansamlinhchi = await Productserver.find({"loaisanpham" : "Nhân Sâm Linh Chi"});
-let dongtrunghathao = await Productserver.find({"loaisanpham" : "Đông Trùng Hạ Thảo"});
-let vienuongtrangda = await Productserver.find({"loaisanpham" : "Viên Uống Trắng Da"});
-let vitaminkhoangchat = await Productserver.find({"loaisanpham" : "Vitamin Khoáng Chất"});
-const ssp = 6;
-botrinao = botrinao.slice(0,ssp);
-collagen = collagen.slice(0,ssp);
-bomat = bomat.slice(0,ssp);
-boxuongkhop = boxuongkhop.slice(0,ssp);
-duongtieuhoa = duongtieuhoa.slice(0,ssp);
-giamcanantoan = giamcanantoan.slice(0,ssp);
-sinhlynam = sinhlynam.slice(0,ssp);
-sinhlynu = sinhlynu.slice(0,ssp);
-tieuduongmomau = tieuduongmomau.slice(0,ssp);
-thannieu = thannieu.slice(0,ssp);
-timmachhuyetap = timmachhuyetap.slice(0,ssp);
-tangcanantoan = tangcanantoan.slice(0,ssp);
-viemduonghohap = viemduonghohap.slice(0,ssp);
-nhansamlinhchi = nhansamlinhchi.slice(0,ssp);
-dongtrunghathao = dongtrunghathao.slice(0,ssp);
-vienuongtrangda = vienuongtrangda.slice(0,ssp);
-vitaminkhoangchat= vitaminkhoangchat.slice(0,ssp);
-comchotrebiengan= comchotrebiengan.slice(0,ssp);
-boganmat= boganmat.slice(0,ssp);
-bomoctoc= bomoctoc.slice(0,ssp);
+	let botrinao = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Bổ Trí Não" } },
+		{ $limit: ssp }
+	])
 
-res.render('users/forclient/index',{
-	botrinao : botrinao,
-	collagen : collagen,
-	bomat : bomat,
-	boxuongkhop : boxuongkhop,
-	duongtieuhoa : duongtieuhoa,
-	giamcanantoan : giamcanantoan,
-	sinhlynam : sinhlynam,
-	sinhlynu : sinhlynu,
-	tieuduongmomau : tieuduongmomau,
-	thannieu : thannieu,
-	timmachhuyetap : timmachhuyetap,
-	tangcanantoan : tangcanantoan,
-	viemduonghohap : viemduonghohap,
-	nhansamlinhchi : nhansamlinhchi,
-	dongtrunghathao : dongtrunghathao,
-	vienuongtrangda : vienuongtrangda,
-	vitaminkhoangchat: vitaminkhoangchat,
-	comchotrebiengan: comchotrebiengan,
-	boganmat: boganmat,
-	bomoctoc: bomoctoc
-});
+	let bomat = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Bổ Mắt" } },
+		{ $limit: ssp }
+	])
+
+	let boxuongkhop = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Bổ Xương Khớp" } },
+		{ $limit: ssp }
+	])
+
+	let duongtieuhoa = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Đường Tiêu Hóa" } },
+		{ $limit: ssp }
+	])
+
+	let giamcanantoan = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Giảm Cân An Toàn" } },
+		{ $limit: ssp }
+	])
+
+	let sinhlynam = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Sinh Lý Nam" } },
+		{ $limit: ssp }
+	])
+	let sinhlynu = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Sinh Lý Nữ" } },
+		{ $limit: ssp }
+	])
+	let tieuduongmomau = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Tiểu Đường Mỡ Máu" } },
+		{ $limit: ssp }
+	])
+	let thannieu = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Thận Niệu" } },
+		{ $limit: ssp }
+	])
+	let timmachhuyetap = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Tim Mạch Huyết Áp" } },
+		{ $limit: ssp }
+	])
+	let tangcanantoan = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Tăng Cân An Toàn" } },
+		{ $limit: ssp }
+	])
+	let viemduonghohap = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Viêm Đường Hô Hấp" } },
+		{ $limit: ssp }
+	])
+	let nhansamlinhchi = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Nhân Sâm Linh Chi" } },
+		{ $limit: ssp }
+	])
+	let dongtrunghathao = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Đông Trùng Hạ Thảo" } },
+		{ $limit: ssp }
+	])
+	let vienuongtrangda = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Viên Uống Trắng Da" } },
+		{ $limit: ssp }
+	])
+	let vitaminkhoangchat = await Productserver.aggregate([
+		{ $match: { "loaisanpham": "Vitamin Khoáng Chất" } },
+		{ $limit: ssp }
+	])
+
+	res.render('users/forclient/index', {
+		botrinao: botrinao,
+		collagen: collagen,
+		bomat: bomat,
+		boxuongkhop: boxuongkhop,
+		duongtieuhoa: duongtieuhoa,
+		giamcanantoan: giamcanantoan,
+		sinhlynam: sinhlynam,
+		sinhlynu: sinhlynu,
+		tieuduongmomau: tieuduongmomau,
+		thannieu: thannieu,
+		timmachhuyetap: timmachhuyetap,
+		tangcanantoan: tangcanantoan,
+		viemduonghohap: viemduonghohap,
+		nhansamlinhchi: nhansamlinhchi,
+		dongtrunghathao: dongtrunghathao,
+		vienuongtrangda: vienuongtrangda,
+		vitaminkhoangchat: vitaminkhoangchat,
+		comchotrebiengan: comchotrebiengan,
+		boganmat: boganmat,
+		bomoctoc: bomoctoc
+	});
 };
 
 
-module.exports.search = async function(req, res){
-let q = tolink.chuyenthanhlink(req.query.q);
+module.exports.search = async function (req, res) {
+	let q = tolink.chuyenthanhlink(req.query.q);
 
-let productserver = await Productserver.find();
+	let productserver = await Productserver.find();
 
-let sanpham = productserver.filter(function(item){
-let tg = tolink.chuyenthanhlink(item.tensanpham)
-return tg.indexOf(q)!== -1;
-})
+	let sanpham = productserver.filter(function (item) {
+		let tg = tolink.chuyenthanhlink(item.tensanpham)
+		return tg.indexOf(q) !== -1;
+	})
 
-res.render('users/forclient/search',{
-	sanpham: sanpham
-});
+	res.render('users/forclient/search', {
+		sanpham: sanpham
+	});
 };
 
-module.exports.googlesearchconsole = function(req, res){
-res.send('google-site-verification: google17f04c91bb3db137.html')
+module.exports.googlesearchconsole = function (req, res) {
+	res.send('google-site-verification: google17f04c91bb3db137.html')
 };
 
